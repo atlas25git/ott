f7e725b51c8bd5ce4f1c8d9efffe53ed36ba0010
lalaisup <44550052+lalaisup@users.noreply.github.com>
2020-08-24 11:31:22 +0530

    Updates

---
+++ precache-manifest.29639474b47d178d20bac488c9cc0da9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7cef2ae91a51e901dfbad015513f39",
+    "url": "/ott/index.html"
+  },
+  {
+    "revision": "96aaa55d7529577d8d44",
+    "url": "/ott/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7102aaee0669d4578c09",
+    "url": "/ott/static/css/main.5d3885c8.chunk.css"
+  },
+  {
+    "revision": "96aaa55d7529577d8d44",
+    "url": "/ott/static/js/2.89f607b6.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/ott/static/js/2.89f607b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7102aaee0669d4578c09",
+    "url": "/ott/static/js/main.96d30470.chunk.js"
+  },
+  {
+    "revision": "a6907bac2e716e52c5d8",
+    "url": "/ott/static/js/runtime-main.59efa96f.js"
+  }
+]);
